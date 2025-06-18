501e2aaf027acf72533e43904bf59a0024b8cfba
Veronica Scerra <vscerra@gmail.com>
2025-06-18 11:53:15 -0700

    updating gridworld class and navigation functions

--- utils/gridworld.py
+++ utils/gridworld.py
@@ -5,7 +5,7 @@ class Gridworld:
         self.size = size
         self.n_states = size * size
         self.start = start
-        self.goal = goal
+        self.goal = goal if goal is not None else self.n_states - 1
         self.traps = traps if traps else []
         self.gamma = gamma
         self.reset()
@@ -55,4 +55,34 @@ class Gridworld:
 
         self.state = next_state
         return next_state, reward, done
+    
+    def get_transition_model(self):
+        """ 
+        Returns a dict[state][action] = list of (prob, next_state, reward)
+        """
+        model = {}
+        for s in range(self.n_states):
+            model[s] = {}
+            row, col = self.state_to_pos(s)
+            for a in self.get_actions():
+                outcomes = []
+                for action_option, prob in zip([a, (a+1)%4, (a+3)%4], [0.8, 0.1, 0.1]):
+                    dr, dc = {0:(-1,0), 1:(0,1), 2:(1,0), 3:(0,-1)}[action_option]
+                    new_row = np.clip(row + dr, 0, self.size - 1)
+                    new_col = np.clip(col + dc, 0, self.size - 1)
+                    s_next = self.pos_to_state((new_row, new_col))
+
+                    if s_next == self.goal:
+                        reward = 10
+                        done = True
+                    elif s_next in self.traps:
+                        reward = -10
+                        done = True
+                    else:
+                        reward = -1
+                        done = False
+                    
+                    outcomes.append((prob, s_next, reward))
+                model[s][a] = outcomes
+        return model
     
--- utils/gridworld_solver.py
+++ utils/gridworld_solver.py
@@ -1,27 +1,24 @@
 import numpy as np
 
-def one_step_lookahead(state, V, env):
+def one_step_lookahead(state, V, P, gamma):
     """
     Computes action-values for all actions from a given state.
     """
-    action_values = np.zeros(len(env.get_actions()))
-    for a in env.get_actions():
-        total = 0
-        for _ in range(10):  # Monte Carlo sampling to estimate transitions
-            env.state = state
-            next_state, reward, _ = env.step(a)
-            total += reward + env.gamma * V[next_state]
-        action_values[a] = total / 10
-    return action_values
-
+    A = np.zeros(len(P[state]))
+    for a in range(len(P[state])):
+        for prob, next_s, reward in P[state][a]:
+            A[a] += prob * (reward + gamma * V[next_s])
+    return A
 
 def value_iteration(env, theta=1e-4):
     V = np.zeros(env.n_states)
+    P = env.get_transition_model()
+    gamma = env.gamma
+
     while True:
         delta = 0
         for s in range(env.n_states):
-            env.state = s
-            A = one_step_lookahead(s, V, env)
+            A = one_step_lookahead(s, V, P, gamma)
             best_action_value = np.max(A)
             delta = max(delta, abs(best_action_value - V[s]))
             V[s] = best_action_value
@@ -31,8 +28,7 @@ def value_iteration(env, theta=1e-4):
     # Derive deterministic policy from value function
     policy = np.zeros(env.n_states, dtype=int)
     for s in range(env.n_states):
-        env.state = s
-        A = one_step_lookahead(s, V, env)
+        A = one_step_lookahead(s, V, P, gamma)
         policy[s] = np.argmax(A)
     return V, policy
 
